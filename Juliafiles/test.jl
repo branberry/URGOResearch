852455c9a9bba457ac0902e4648a44c0ac41e8a9
wowbob396 <wowbob396@gmail.com>
2018-06-07 20:05:34 -0500

    created beam animation

--- Juliafiles/test.jl
+++ Juliafiles/test.jl
@@ -1,4 +1,22 @@
 using Plots
+function addBlock(f,section)
+    # Define the number of subintervals where the block is applying force
+    dist = ceil(block_length/h)
+
+    # Define the starting distance of the block:
+    start = section
+    trunc(Int,start);
+
+    # Define the segment the block is being applied to:
+    finish = start + dist;
+
+    trunc(Int, finish);
+    for i = trunc(Int,start):trunc(Int, finish)
+        # I divide the block_mass by dist because dist represents the number of segments
+        # The block is being applied to.
+        f[i] = -h^4/(E*I) * (w +  block_mass_per_unit);
+    end
+end
 
 L = 10; # length = 10 m
 b = 0.1; # width = 10 cm
@@ -37,6 +55,8 @@ for i=3:(N - 2)
     A[i,i-2] = A[i,i+2] = 1; 
 end
 
+
+
 # Left end
 A[1,1] = 1;
 A[2,2] = 7;
@@ -46,13 +66,14 @@ A[2,1] = 0;
 A[3,1] = 0;
 A[2,3] = -4;
 A[2,4] = 1;
+
 # Right end
 A[N,N] = 1;
-A[N-1,N-1] =  7;
-A[N-1,N] = 0;
-A[N-2,N] = 0;
-A[N, N-1] = 0;
-A[N, N-2] = 0;
+A[N-1,N-1] =  5;
+A[N-1,N] = -2;
+A[N-2,N] = 1;
+A[N, N-1] = -2;
+A[N, N-2] = 1;
 A[N-1,N-2] = -4;
 A[N-1,N-3] = 1;
 # Solve for y
@@ -61,7 +82,7 @@ x = ones(N,1);
 
 x = collect(0:h:L)
 
-y_exact = -b*d*rho*g/(24*E*I)*x.^2.*(L - x).^2;
+y_exact = -b*d*rho*g/(24*E*I)*x.^2.*(6.*L^2 - 4.*L*x + x.^2);
 ErrMax = maximum(abs.(y-y_exact))
 print("\n")
 display(ErrMax)
@@ -70,7 +91,31 @@ print("\n")
 # this method uses the GR plotting backend
 gr()
 
-p1 = scatter(x,y);
-p2 = plot(x,abs.(y-y_exact), title="Error");
-plot(p1,p2,layout=(2,1))
-plot!(x,y_exact, title="Both ends fixed displacement")
+# final starting point for block to be placed on board
+plot(x,y,ylim=(-3.5,0))
+last = trunc(Int,n-(n/10))
+print(last)
+print("\n\n")
+
+@gif for j=1:last
+    f = -h^4/(E*I) * w * ones(N, 1);
+    f[1] = 0;
+    f[N] = 0;
+    # Creating diagonals of the matrix
+    for i=3:(N - 2)
+        A[i,i] = 6;
+        A[i,i-1] = A[i,i+1] = -4;
+        A[i,i-2] = A[i,i+2] = 1; 
+    end
+    addBlock(f,j)
+    y = A\f;
+
+    plot(x,y,ylim=(-3.5,0))
+end every 1
+
+
+#p1 = scatter(x,y);
+#p2 = plot(x,abs.(y-y_exact), title="Error");
+#plot(p1,p2,layout=(2,1))
+#plot!(x,y_exact, title="Both ends fixed displacement")
+
