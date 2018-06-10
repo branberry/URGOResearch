020a467ae35f3516abc198127ac69c6eb97801a3
wowbob396 <wowbob396@gmail.com>
2018-06-09 23:43:51 -0500

    created the proper animations

--- Juliafiles/animations.jl
+++ Juliafiles/animations.jl
@@ -1,4 +1,22 @@
 using Plots
+function addBlock(f,section)
+    # Define the number of subintervals where the block is applying force
+    dist = ceil(block_length/h)
+
+    # Define the starting distance of the block:
+    start = trunc(Int,section);
+
+    # Define the segment the block is being applied to:
+    finish = trunc(Int, start + dist);
+
+    for i = trunc(Int,start):trunc(Int, finish)
+        # I divide the block_mass by dist because dist represents the number of segments
+        # The block is being applied to.
+        f[i] = -h^4/(E*I) * (w +  block_mass_per_unit);
+    end
+
+    return [start finish]';
+end
 
 L = 10; # length = 10 m
 b = 0.1; # width = 10 cm
@@ -37,6 +55,8 @@ for i=3:(N - 2)
     A[i,i-2] = A[i,i+2] = 1; 
 end
 
+
+
 # Left end
 A[1,1] = 1;
 A[2,2] = 7;
@@ -46,32 +66,35 @@ A[2,1] = 0;
 A[3,1] = 0;
 A[2,3] = -4;
 A[2,4] = 1;
+
 # Right end
 A[N,N] = 1;
-A[N-1,N-1] =  7;
-A[N-1,N] = 0;
-A[N-2,N] = 0;
-A[N, N-1] = 0;
-A[N, N-2] = 0;
+A[N-1,N-1] =  5;
+A[N-1,N] = -2;
+A[N-2,N] = 1;
+A[N, N-1] = -2;
+A[N, N-2] = 1;
 A[N-1,N-2] = -4;
 A[N-1,N-3] = 1;
 # Solve for y
 y = A\f;
+x = ones(N,1);
 
 x = collect(0:h:L)
 
-y_exact = -b*d*rho*g/(24*E*I)*x.^2.*(L - x).^2;
+y_exact = -b*d*rho*g/(24*E*I)*x.^2.*(6.*L^2 - 4.*L*x + x.^2);
 ErrMax = maximum(abs.(y-y_exact))
+print("\n")
+display(ErrMax)
+print("\n")
 
+# this method uses the GR plotting backend
 gr()
 
-# initialize a 3D plot with 1 empty series
-plt = path3d(1, xlim=(0,10), ylim=(-.01,0), zlim=(-.05,0),
-                xlab = "Meters", ylab = "Displacement", zlab = "z",
-                title = "Both Ends Fixed (Class Project)", marker = 1)
+blockPoints = ones(N,1);
+blockPoints[10] = -2;
+blockPoints[20] = -3;
 
-# build an animated gif, saving every 10th frame
-@gif for i=1:N
-    z = 0;
-    push!(plt, x[i],z , y[i])
-end every 10
+# final starting point for block to be placed on board
+plot(x,y,ylim=(-3.5,0),ylab="Displacement (m)",xlab="Beam (m)",title="Block on Beam")
+scatter!(x,blockPoints)
